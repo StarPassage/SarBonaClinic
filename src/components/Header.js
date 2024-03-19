8bec0303ccb08948c3497c86c86f5726005ed099
David <davemarti02@gmail.com>
2024-03-19 04:03:58 +0400

    updated Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,10 @@ import { useState } from "react";
 const Header = () => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
   return (
     <header className="sticky top-0 bg-white text-black md:py-4 z-[99]">
       <div className="container mx-auto flex flex-wrap justify-between items-center p-4 md:p-0">
@@ -17,7 +21,7 @@ const Header = () => {
         </Link>
 
         {/* Appointment button for mobile, centered */}
-        <div className="md:hidden flex flex-grow justify-center mr-5">
+        <div className="lg:hidden flex flex-grow justify-center mr-5">
           <Link
             href="#appointment"
             className="px-4 py-4 bg-light-golden text-white rounded hover:bg-dark-golden transition duration-300"
@@ -27,22 +31,51 @@ const Header = () => {
         </div>
 
         {/* Hamburger menu button */}
-        <button
-          className="md:hidden text-3xl"
-          onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
-        >
-          &#9776;
-        </button>
+        <div className="lg:hidden z-50">
+          <button
+            onClick={toggleMobileMenu}
+            className={`flex flex-col h-10 w-10 justify-around items-center p-2 relative focus:outline-none ${
+              isMobileMenuOpen ? "open" : ""
+            }`}
+          >
+            {/* Hamburger Icon */}
+            <span
+              className={`block w-full h-0.5 bg-black transform transition duration-500 ease-in-out ${
+                isMobileMenuOpen ? "rotate-45 translate-y-1.5" : ""
+              }`}
+            />
+            <span
+              className={`block w-full h-0.5 bg-black transform transition duration-500 ease-in-out ${
+                isMobileMenuOpen ? "opacity-0" : ""
+              }`}
+            />
+            <span
+              className={`block w-full h-0.5 bg-black transform transition duration-500 ease-in-out ${
+                isMobileMenuOpen ? "-rotate-45 -translate-y-2.5" : ""
+              }`}
+            />
+          </button>
+        </div>
 
         {/* Mobile Menu */}
         <div
-          className={`md:flex w-full md:w-auto ${
+          className={`lg:flex w-full lg:w-auto ${
             isMobileMenuOpen ? "block" : "hidden"
           }`}
         >
-          <nav className="w-full md:flex md:items-center md:justify-between">
-            <ul className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4 w-full">
-              <HeaderServices />
+          <nav className="w-full lg:flex lg:items-center lg:justify-between">
+            <ul className="flex flex-col lg:flex-row space-y-4 lg:space-y-0 lg:space-x-4 w-full">
+              <li className="lg:hidden">
+                <Link
+                  href="#services"
+                  className="transition-colors duration-300 hover:text-light-golden flex items-center px-5 py-5 text-3xl md:text-base"
+                >
+                  Услуги
+                </Link>
+              </li>
+              <div className="hidden lg:block">
+                <HeaderServices />
+              </div>
               <li className="group relative">
                 <a
                   href="#about"
@@ -67,7 +100,7 @@ const Header = () => {
                   Врачи
                 </a>
               </li>
-              <li className="group relative">
+              <li className="group relative hidden xl:block">
                 <a
                   href="#certificates"
                   className="transition-colors duration-300 hover:text-light-golden flex items-center px-5 py-5 text-3xl md:text-base"
@@ -75,7 +108,7 @@ const Header = () => {
                   Сертификаты
                 </a>
               </li>
-              <li className="group relative">
+              <li className="group relative hidden xl:block">
                 <a
                   href="#reviews"
                   className="transition-colors duration-300 hover:text-light-golden flex items-center px-5 py-5 text-3xl md:text-base"
@@ -94,7 +127,7 @@ const Header = () => {
             </ul>
           </nav>
         </div>
-        <div className="hidden md:flex">
+        <div className="hidden lg:flex">
           <Link
             href="#appointment"
             className="px-4 py-4 bg-light-golden text-white rounded hover:bg-dark-golden transition duration-300"
