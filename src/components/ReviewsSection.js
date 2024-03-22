70857296e2378015bc3d5259fc471db7a2faa6a6
David <davemarti02@gmail.com>
2024-03-22 23:43:20 +0400

    added Reviews anchor

--- src/components/ReviewsSection.js
+++ src/components/ReviewsSection.js
@@ -84,7 +84,7 @@ const ReviewsSection = () => {
   };
 
   return (
-    <div className="bg-white">
+    <div id="reviews" className="bg-white">
       <h1 className="text-5xl md:text-5xl lg:text-6xl text-slate-700 text-left ml-[6%] mr-[4%] lg:mr-[52%] pt-[0.5em] pb-[2%] md:pb-0">
         Отзывы
       </h1>
